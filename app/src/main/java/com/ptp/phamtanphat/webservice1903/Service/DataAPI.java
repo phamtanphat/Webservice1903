07edd27464f47f0e3d579e93a38636b29e1689f0
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-04 20:59:18 +0700

    "Doc du lieu giao dien"

--- app/src/main/java/com/ptp/phamtanphat/webservice1903/Service/DataAPI.java
+++ app/src/main/java/com/ptp/phamtanphat/webservice1903/Service/DataAPI.java
@@ -6,8 +6,15 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface DataAPI {
+
     @GET("cacmonan.php")
     Call<List<Monan>> getTatcamonan();
+//    http://10.0.0.16/Monan1903/themmonan.php?Tenmonan=Haca&Giamonan=15000&Diachi=duong3thang2
+    @GET("themmonan.php")
+    Call<String> onInsertMonan(@Query("Tenmonan") String tenmonan ,
+                               @Query("Giamonan") String giamonan ,
+                               @Query("Diachi") String diachi);
 }
