3ce000e8df5064593c282db8cf888ee52eca3d12
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-04 20:41:43 +0700

    "Doc du lieu giao dien"

--- app/src/main/java/com/ptp/phamtanphat/webservice1903/ThemmonanActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/webservice1903/ThemmonanActivity.java
@@ -2,12 +2,26 @@ package com.ptp.phamtanphat.webservice1903;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ThemmonanActivity extends AppCompatActivity {
 
+    EditText edtTenmonan,edtGiamonan,edtDiachi;
+    Button btnHuy, btnXacnhan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_themmonan);
+        anhxa();
+    }
+
+    private void anhxa() {
+        edtTenmonan = findViewById(R.id.edittextTenmonan);
+        edtGiamonan = findViewById(R.id.edittextGiamonan);
+        edtDiachi = findViewById(R.id.edittextDiachimonan);
+        btnHuy = findViewById(R.id.buttonHuy);
+        btnXacnhan = findViewById(R.id.buttonXacnhan);
     }
 }
