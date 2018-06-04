34ecf1bc592bb24d2fbd980135ad431011f827ca
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-04 21:07:47 +0700

    "Them du lieu"

--- app/src/main/java/com/ptp/phamtanphat/webservice1903/ThemmonanActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/webservice1903/ThemmonanActivity.java
@@ -2,9 +2,18 @@ package com.ptp.phamtanphat.webservice1903;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ptp.phamtanphat.webservice1903.Service.DataAPI;
+import com.ptp.phamtanphat.webservice1903.Service.RetrofitAPI;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ThemmonanActivity extends AppCompatActivity {
 
@@ -15,6 +24,41 @@ public class ThemmonanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_themmonan);
         anhxa();
+        btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnXacnhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tenmonan = edtTenmonan.getText().toString();
+                String giamonan = edtGiamonan.getText().toString();
+                String diachi = edtDiachi.getText().toString();
+                if (!tenmonan.isEmpty() && !giamonan.isEmpty() && !diachi.isEmpty()){
+                    DataAPI dataAPI = RetrofitAPI.getData();
+                    Call<String> callback = dataAPI.onInsertMonan(tenmonan,giamonan,diachi);
+                    callback.enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            String ketqua = response.body();
+                            if (ketqua.equals("success")){
+                                Toast.makeText(ThemmonanActivity.this, "Them thanh cong!!", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }else {
+                                Toast.makeText(ThemmonanActivity.this, "Loi!!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+
+                        }
+                    });
+                }
+            }
+        });
     }
 
     private void anhxa() {
